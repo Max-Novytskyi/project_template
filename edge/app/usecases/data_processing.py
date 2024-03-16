cfbe33c745a202953266a7d36f08d97c002424cd
Max Novytskyi <max123novitskiy@gmail.com>
2024-03-16 20:22:58 +0200

    changing data_processing.py in edge module

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -2,6 +2,7 @@ from app.entities.agent_data import AgentData
 from app.entities.processed_agent_data import ProcessedAgentData
 
 
+
 def process_agent_data(
     agent_data: AgentData,
 ) -> ProcessedAgentData:
@@ -13,3 +14,15 @@ def process_agent_data(
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
     # Implement it
+    processed_data = ProcessedAgentData()
+    z_coord = agent_data.z_coord
+    if z_coord < 16550:
+        state = "pothole"
+    elif 16550 < z_coord < 17550:
+        state = "road"
+    else:
+        state = "bump"
+
+    processed_data.agent_data = agent_data
+    processed_data.road_state = state
+    return processed_data
